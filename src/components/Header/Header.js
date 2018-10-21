fabdcc010d894ee1b753e641909ac382c4190984
nachomac12 <macias.ignacio@hotmail.com>
2018-10-21 19:09:20 -0300

    navegacion a /

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
         <div className="container">
-          <h3 className="text-white pt-1">Tu-Canchita Admin</h3>
+        <Link className="navbar-brand" to="/"><h3 className="text-white pt-1">Tu-Canchita Admin</h3></Link>
         </div>
       </nav>
     );
